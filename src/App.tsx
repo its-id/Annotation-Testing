175502c7aabbaba68067de7f11eb66a52f959927
Indrakant Dana <ik.dana24@gmail.com>
2023-05-07 19:55:48 +0530

    update Workspace

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
-import { Canvas } from "./components/canvas/typescript/canvas";
+// import { Canvas } from "./components/canvas/typescript/canvas";
+import Layout from "antd/lib/layout";
+import AnnotationPageComponent from "./components/annotation-page/annotation-page";
 
 function App() {
   return (
     <div className="App">
       {/* import class canvas and display here */}
-      
+      <Layout>
+        <Layout.Content style={{ height: "100%" }}>
+          <AnnotationPageComponent />
+        </Layout.Content>
+      </Layout>
     </div>
   );
 }
